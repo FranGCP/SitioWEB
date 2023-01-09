ac4c0908beda520ce4bee39d2ac532cc02b79de2
Francisco Gabriel Contreras Palavecino <franciscogabrielcopal@gmail.com>
2023-01-09 20:25:35 -0300

    Proyecto final

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var nosotrosRouter = require('./routes/nosotros');
 var contactoRouter = require('./routes/contacto');
 var clientesRouter = require('./routes/clientes');
+var novedadesRouter = require('./routes/novedades');
+var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades')
 
 var app = express();
 
@@ -23,10 +27,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'PW2022awqyeudj',
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario); 
+    if (req.session.id_usuario) {
+      next ();
+    } else {
+      res.redirect('/admin/login');
+    }
+  } catch (error){
+    console.log(error);
+  }
+}
+
+
 app.use('/', indexRouter);
 app.use('/nosotros', nosotrosRouter);
 app.use('/contacto', contactoRouter);
 app.use('/clientes', clientesRouter);
+app.use('/novedades', novedadesRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
